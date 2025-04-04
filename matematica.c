9d70dbfabe854c28210393d2314ea57ecfd95371
ProgramacaoGHub <kaueandradre416@gmail.com>
2025-04-04 19:59:05 +0000

    AS 3 OPERAÇOES

--- matematica.c
+++ matematica.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int main() {
+     //OPERADORES DE ATRUbUIÇÃO
+     int numero1 ,numero2 ,numero3=50, resultado ;
+     int soma, subtracao, divisao,multiplicacao;
+
+     //contas
+     resultado = numero1 ;
+     printf ("digite o numero inicial:" );
+     scanf ("%d", &numero1);
+
+
+     resultado +=numero2 ;
+     printf( "digite o segundo numero:");
+     scanf("%d" , &numero2 );
+     
+     //operção matematica
+     soma = numero1 + numero2 -numero3;
+
+     printf("A soma é: %d\n", soma);
+
+     resultado-= numero3;
+     
+    // printf("o resultado é: %d\n" );
+
+
+
+
+
+    return 0;
+}
